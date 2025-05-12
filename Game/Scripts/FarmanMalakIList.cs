a11119638c67992553b1598c6e5a3ebf2cc544e0
Hasan Farman <154061344+skelebone01@users.noreply.github.com>
2025-05-12 19:55:19 +0300

    Delete Game/Scripts directory

--- Game/Scripts/FarmanMalakIList.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-//an interface class for linked list and double linked list
-public interface IList
-{
-    Node head { get; }
-    void BuildList(Transform[] transforms, GameObject[] visuals);
-    Node FindByIndex(int index);
-
-}
